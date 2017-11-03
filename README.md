71b8e13c16b382974a9884813c587f4b56e8242c
Stefano J. Attardi <stefano@attardi.org>
2017-11-03 13:38:08 -0700

    Add support for JSX templates and specifying JS extension
    Closes #1
    Closes #2

--- README.md
+++ README.md
@@ -65,7 +65,7 @@ You can even specify which props to pass through from the parent:
 > vectorinox --jsx --stdout vector-file-to-optimize.svg --jsxTag View --jsxProp component=svg --jsxInheritProp stroke --jsxInheritProp fill
 ```
 ```xml
-<View stroke="#444444" strokeWidth={2} fill="none" component="svg" stroke={stroke} fill={fill}>
+<View stroke={stroke} strokeWidth={2} fill={fill} component="svg">
   <rect x="#CFE9FF" cx="M22.4375,10 L35.5625,10" />
   <circle cx="M22.4375,19 L35.5625,19" />
 </View>
@@ -81,3 +81,27 @@ Or which props object to splice in:
   <circle cx="M22.4375,19 L35.5625,19" />
 </View>
 ```
+
+If you use the `--jsx` option without `--stdout`, a `.js` file with the same name as the `.svg` file will be created. To customize the extension used, use `--jsxExtension tsx`.
+
+React Component Templates
+-------------------------
+
+It usually takes a small amount of boilerplate to convert an SVG image to a valid React component. Vectorinox comes with a default template used when converting to JSX. It looks like this:
+
+```js
+import * as React from 'react';
+
+const %NAME% = (%PROPS%) =>
+  %SVG%;
+
+export default %NAME%;
+```
+
+You can customize the template by providing a `--jsxTemplate template.js` option, or by creating a file called `.svgTemplate.js` in the current directory.
+
+Available placeholders are:
+
+- `%NAME%`: a CamelCased version of the SVG file name.
+- `%PROPS%`: an object destructuring of props, inferred from the `--jsxInheritProp` and `--jsxSpliceProp` options provided. For example, with `--jsxInheritProp color --jsxInheritProp fill --jsxSpliceProp props`, this token gets replaced with `{color, fill, ...props}`.
+- `%SVG%`: the actual converted SVG markup. Put this on its own line with space in front of it to indent the code accordingly.
--- src/bin.ts
+++ src/bin.ts
@@ -1,15 +1,26 @@
 #!/usr/bin/env node
 
 import * as fs from 'fs';
+import * as path from 'path';
 import {clean, cleanJSX} from './index';
+
+const DEFAULT_JSX_TEMPLATE = `import * as React from 'react';
+
+const %NAME% = (%PROPS%) =>
+  %SVG%;
+
+export default %NAME%;
+`;
 
 const options = {
   jsx: false,
   stdout: false,
   jsxTag: 'svg',
+  jsxExtension: 'js',
   jsxProps: {} as {[name: string]: string},
   jsxInheritProps: [] as string[],
   jsxSpliceProps: [] as string[],
+  jsxTemplate: '.svgTemplate.js',
 };
 
 let nextOption: keyof typeof options | null = null;
@@ -22,6 +33,8 @@ const files = process.argv.slice(2).filter(arg => {
       options.jsxInheritProps.push(arg);
     } else if (nextOption == 'jsxSpliceProps') {
       options.jsxSpliceProps.push(arg);
+    } else if (nextOption == 'jsxExtension') {
+      options.jsxExtension = arg;
     } else {
       options[nextOption] = arg;
     }
@@ -52,18 +65,57 @@ const files = process.argv.slice(2).filter(arg => {
     nextOption = 'jsxSpliceProps';
     return false;
   }
+  if (arg === '--jsxExtension') {
+    nextOption = 'jsxExtension';
+    return false;
+  }
   return true;
 });
 
+function indent(s: string, prefix: string) {
+  return s.split('\n').join(`\n${prefix}`);
+}
+
+function wrapJSX(pathName: string, cleaned: string, template: string) {
+  const name = path
+    .basename(pathName, path.extname(pathName))
+    .replace(/(^|-)([a-z])/g, (_, _1, c) => c.toUpperCase());
+  const propsDefinition = [...options.jsxInheritProps, ...options.jsxSpliceProps.map(p => `...${p}`)].join(
+    ', ',
+  );
+  return template
+    .replace(/%NAME%/g, name)
+    .replace(/%PROPS%/g, `{${propsDefinition}}`)
+    .replace(
+      /(\n([ \t]*))?%SVG%/g,
+      (_, before, whitespace) => (before || '') + indent(cleaned, whitespace || '  '),
+    );
+}
+
 for (let f of files) {
-  if (!options.stdout) console.error(f);
   const data = fs.readFileSync(f).toString();
-  const cleaned = options.jsx
+  let cleaned = options.jsx
     ? cleanJSX(data, options.jsxTag, options.jsxProps, options.jsxInheritProps, options.jsxSpliceProps)
     : clean(data);
+
+  if (options.jsx) {
+    const template = fs.existsSync(options.jsxTemplate)
+      ? fs.readFileSync(options.jsxTemplate).toString()
+      : DEFAULT_JSX_TEMPLATE;
+    cleaned = wrapJSX(f, cleaned, template);
+  }
+
   if (options.stdout) {
     console.log(cleaned);
   } else {
+    if (options.jsx) {
+      const sourceName = f;
+      f = f.replace(/\.svg$/, `.${options.jsxExtension}`);
+      console.error(`${sourceName} -> ${f}`);
+    } else {
+      console.error(f);
+    }
+
     fs.writeFileSync(f, cleaned);
   }
 }
